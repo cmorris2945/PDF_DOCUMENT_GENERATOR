f1ee6555f4ad2d77428362f0c1cc57b05806eb5c
cmorris2945 <cmorris2945@yahoo.com>
2023-06-12 15:12:57 -0400

    Day 24: Added more pages for each topic

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fpdf import FPDF
 import pandas as pd
 
 pdf = FPDF(orientation="P", unit="mm", format="A4")
+pdf.set_auto_page_break(auto=False, margin=0)
 
 df = pd.read_csv("topics (2).csv")
 
@@ -14,8 +15,20 @@ for index, row in df.iterrows():
              ln=1)
     pdf.line(10, 21, 200, 21)
 
+    pdf.ln(265)
+
+    ## Setting the footer...
+    pdf.set_font(family="Times", style="I", size=8)
+    pdf.set_text_color(180,180,180)
+    pdf.cell(w=0,h=10, txt=row["Topic"],align="R")
+
     for i in range(row["Pages"]-1):
         pdf.add_page()
+        ## Setting the header...
+        pdf.ln(277)
+        pdf.set_font(family="Times", style="I", size=8)
+        pdf.set_text_color(180, 180, 180)
+        pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
 
 pdf.output("output.pdf")
 
